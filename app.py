823e2186bc6347df15d257373fc582dee8e8065f
jenkins_test <jenkins@example.com>
2022-01-08 21:26:56 +0200

    hello from Jenkins

--- app.py
+++ app.py
@@ -20,5 +20,5 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello from Docker! I have been seen {} times.\n'.format(count)
+    return 'Hello from Jenkins but not ftom Docker! I have been seen {} times.\n'.format(count)
 
