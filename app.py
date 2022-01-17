41f5f70f96266a3e238a555609ae318ea2258ecf
exskandr <57629837+exskandr@users.noreply.github.com>
2022-01-17 11:01:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -21,5 +21,5 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello from Jenkins and GIT but not from Docker! I have been seen {} times.\n'.format(count)
+    return 'Hello from Jenkins and GIT and NOW from Docker! I have been seen {} times.\n'.format(count)
 
